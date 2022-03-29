efdbe23bcb5cb865a1943e12f779b2c7339111d7
Sudesh Bhat <48199720+bhatsudesh@users.noreply.github.com>
2022-03-29 11:59:29 +0530

    Update App.js
    Optimal way to capitalize

--- src/App.js
+++ src/App.js
@@ -2,17 +2,22 @@ import "./styles.css";
 
 export default function App() {
   const capitalize = (str) => {
+    // toUpperCase
     str = str.toLowerCase();
     let words = str
       .split(" ")
-      .map((w) => w.replace(/^\w/, (c) => c.toUpperCase()));
+      .map((w) => `${w.charAt(0).toUpperCase()}${w.substring(1)}`);
     return words.join(" ");
   };
   return (
     <div className="App">
       <h1>{capitalize("heLLo codeSandbox")}</h1>
-      <div className="box">A</div>
-      <div className="box">B</div>
+      <div className="container">
+        <div className="box">A</div>
+        <div className="box">B</div>
+        <div className="box">C</div>
+        <div className="box">D</div>
+      </div>
     </div>
   );
 }
